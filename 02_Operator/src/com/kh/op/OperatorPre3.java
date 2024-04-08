014625b71b23b4851e4fda332886924fdb603546
kyungnankimKHT <165994108+kyungnankimKHT@users.noreply.github.com>
2024-04-08 12:43:48 +0900

    Operator

---
+++ 02_Operator/src/com/kh/op/OperatorPre3.java
@@ -0,0 +1,21 @@
+package com.kh.op;
+
+public class OperatorPre3 {
+	//public static void method1() 을 만들어서
+	// Scanner 를 이용해 num1 num2 의 값을 입력받고
+	// < > 를 사용해서 입력 받은 값이 < > 중 어떤 값에서 true 가 나오는지 확인하기
+	// boolean result1 = num1 > num2;
+	// boolean result2 = num1 >= num2;
+	// boolean result3 = num1 < num2;
+	// boolean result4 = num1 <= num2;
+	
+	// 응용편 
+	//public static void method2() num1 num2 num3 를 받고 
+	// && || 사용해서 크고 작은 값 확인하기
+	// boolean result = (num1 < num2) && (num2 == num3) 참이 되는가?
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
