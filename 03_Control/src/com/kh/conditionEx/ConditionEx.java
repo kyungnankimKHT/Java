1278da06813f7930bc3b14c4c3355e1e9fed1fa3
kyungnankimKHT <165994108+kyungnankimKHT@users.noreply.github.com>
2024-04-08 16:15:10 +0900

    control

--- 03_Control/src/com/kh/conditionEx/ConditionEx.java
+++ 03_Control/src/com/kh/conditionEx/ConditionEx.java
@@ -17,20 +17,76 @@ public class ConditionEx {
 	 	자동으로 실행 종료
 	 }
 	 */
-
-	// 메서드 1 : 19세 이상은 성인입니다. 출력하기
-	
 	public static void method1() {
 		Scanner sc = new Scanner(System.in);
 		System.out.print("고객님의 나이를 입력하세요 : " );
 		int age = sc.nextInt();
+		// 만약에 고객의 나이가 19세 이상이라면
+		if (age >= 19) {
+		// age의 값이 19 이상이면 성인입니다 출력되게 하기
+			System.out.println("성인입니다.");
+		} else {
+		// 만약에 age 값이 성인이 아니라면 성인이 아닙니다. 출력하게 하기
+			System.out.println("성인이 아닙니다.");
+		}
+		
 	}
+	
+	public static void method2() {
+		int num = 3;
+		
+		// 만약에 num 이 짝수일 경우  짝수입니다. 출력하기
+		// 짝수 2 % 0
+		if (num % 2 == 0) {
+			System.out.println("짝수입니다!!!");
+		} else {
+			System.out.println("홀수입니다.");
+		}
+		// num 이 홀수일 경우 홀수입니다 출력하기
+		
+		
+	}
+	
+	public static void method3() {
+		int age = 20;
+			//    true        true
+		if (age > 0   && age < 14) {
+			//어린이라고 표기하기
+			System.out.println("어린입니다.");
+		} else {
+			System.out.println("어린이가 아닙니다.");
+		}		
+	}
+	
+	// Scanner 활용해서 진행하기
+	public static void practice1() {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("나이를 입력하세요 : ");
+		
+		int age = sc.nextInt();
+		
+		if (age >= 10 && age < 20) {
+			System.out.println("10대입니다.");
+		} else {
+			System.out.println("10대가 아닙니다.");
+		}
+	}
+	public static void practice2() {
+		int age = 88;
+		if ( age >= 80) {
+			System.out.println("80세 이상입니다.");
+		}
+	}
+
+	
 	
 	
 	// 최종 메서드
 	public static void main(String[] args) {
-		
-
+		//method1();
+		//method2();
+		//method3();
+		practice1();
+		practice2();
 	}
-
 }
