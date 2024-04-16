165475542b499a24bdf4373a907f719d3d15d6b9
kyungnankimKHT <165994108+kyungnankimKHT@users.noreply.github.com>
2024-04-16 16:29:47 +0900

    OOP

---
+++ 05_Oop/src/com/kh/oop/method/GetterSetter.java
@@ -0,0 +1,14 @@
+package com.kh.oop.method;
+/*
+Setter
+	객체의 속성 값을 설정하는 메서드
+	객체 외부에서 속성 값을 변경할 수 있음
+	
+Getter 
+	객체의 속성 값을 반환하는 메서드
+	객체 외부에서 속성 값을 읽을 수 있음
+*/
+public class GetterSetter {
+	
+
+}
--- 05_Oop/src/com/kh/oop/method/Student.java
+++ 05_Oop/src/com/kh/oop/method/Student.java
@@ -8,26 +8,47 @@ public class Student {
 	private int grade;
 	//메서드
 	  //생성자 : 기본
+	public Student() {
+		
+	}
 	  //생성자 : 필수 이름 나이 학년
+	public Student(String name, int age, int grade) {
+		this.name = name;
+		this.age = age;
+		this.grade = grade;
+	}
 	  //return
 	  public String getName() {
 		  return name;
 	  }
-	  public [자료형] getAge(){
+	  public int getAge(){
 		  return age;
 	  }
-	  public [자료형] getGrade(){
+	  public int getGrade(){
+		  return grade;
 	  }
 	  public void info() {
-		  System.out.println("학생의 이름 : " + name);
-		  System.out.println("학생의 나이 : " + age);
-		  System.out.println("학생의 학년 : " +     );
+		  System.out.println("학생의 이름 : " +  name);
+		  System.out.println("학생의 나이 : " +   age);
+		  System.out.println("학생의 학년 : " + grade);
 	  }
 	  //메인메서드
 	public static void main(String[] args) {
+		Student student1 = new Student("박영수", 15, 2);
+		Student student2 = new Student("김영희", 17, 1);
+		student1.info();
+		student2.info();
 		//student1 "박영수" 15 2
 		//student2 "김영희" 17 1
 
 	}
 
 }
+
+
+
+
+
+
+
+
