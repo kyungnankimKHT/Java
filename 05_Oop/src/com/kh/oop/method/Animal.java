f786334d4361ef83fc073a44f61aa46b48c11486
kyungnankimKHT <165994108+kyungnankimKHT@users.noreply.github.com>
2024-04-16 15:22:59 +0900

    OOP

---
+++ 05_Oop/src/com/kh/oop/method/Animal.java
@@ -0,0 +1,34 @@
+package com.kh.oop.method;
+
+public class Animal {
+//필드
+	public String name;
+	public int age;
+	
+	
+//메서드
+	//생성자
+	// 기본
+	public Animal() {
+	}
+	// 필수 name age
+	
+	//return
+	public String getName() {
+		return name;
+	}
+	
+	// return 으로 나이 받기
+	public [ 자료형넣기 ] getAge() {
+		return [ 변수명 ];
+		
+	}
+	
+	
+	//메인메서드
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
