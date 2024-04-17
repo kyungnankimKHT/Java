c45e1f2b69e2cd7ee1d47628fecf7669607a2ced
kyungnankimKHT <165994108+kyungnankimKHT@users.noreply.github.com>
2024-04-17 09:34:13 +0900

    class

--- 05_Oop/src/com/kh/oop/method/ex/Person.java
+++ 05_Oop/src/com/kh/oop/method/ex/Person.java
@@ -6,6 +6,10 @@ public class Person {
 	private String name;
 	private    int  age;
 	
+	//Person 사람을 담을 생성자
+	public Person() {
+	}
+
 	//필드 대신 값을 저장하고 내보내는 setter getter 생성
 	public void setName(String name) {
 		this.name = name;
@@ -20,8 +24,32 @@ public class Person {
 	public int getAge() {
 		return age;
 	}
-	
 
-	
+	//메인 메서드
+	// 단축키 main ctrl + space
+	public static void main(String[] args) {
+		Person person1 = new Person();
+		
+		//Setter를 활용해서 속성 값 설정
+		person1.setName("홍길동");
+		person1.setAge(20);
+		
+		//Getter를 활용해서 속성값 읽기
+		System.out.println("이름 : " + person1.getName());
+		System.out.println("나이 : " + person1.getAge());
+		
+	}
 	
 }
+
+
+
+
+
+
+
+
+
+
+
+
