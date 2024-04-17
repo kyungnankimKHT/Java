62de89768a81fdf0292821b2c60523b081b84e1f
kyungnankimKHT <165994108+kyungnankimKHT@users.noreply.github.com>
2024-04-17 14:10:31 +0900

    class

--- 05_Oop/src/com/kh/oop/method/oop/DrinkMaker.java
+++ 05_Oop/src/com/kh/oop/method/oop/DrinkMaker.java
@@ -5,17 +5,55 @@ public class DrinkMaker {
 	private String drinkType; //음료종류
 	private    int quantity; //음료 양
 	private    int temp;    // 온도
-//메서드
+
+	//메서드
 	//setter
+	public void setDrinkType(String drinkType) {
+		this.drinkType = drinkType;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	public void setTemp(int temp) {
+		this.temp = temp;
+	}
 	//getter
+	public String getDrinkType() {
+		return drinkType;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public int getTemp() {
+		return temp;
+	}
 	//생성자 : 기본
+	public DrinkMaker() {
+	}
+
 	//생성자 : 필수 음료종류 음료양 온도
-	//void makeDrink
-		//음료종류 음료양
-		// if 온도 (<5) 차갑게  
-		// else if 온도 (5 ~ 10 )미지근하게
-		// else { 뜨겁게
-	//if - else if - else 탈출 후 
-	//음료제조 완료 출력
+	public DrinkMaker(String drinkType, int quantity, int temp ) {
+		this.drinkType = drinkType;
+		this.quantity = quantity;
+		this.temp = temp;
+	}
+	public void makeDrink() {
+		System.out.println("주문 음료 : " + drinkType);
+		System.out.println("음료 양  : " + quantity);
+		if (temp < 5) {
+			System.out.println("차갑게");
+		} else if(temp > 5  &&  temp < 10) {
+			System.out.println("미지근하게");
+		} else {
+			System.out.println("뜨겁게");
+		}
+	}
+
 	
 }
+
+
+
+
+
+
