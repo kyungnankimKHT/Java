015c107d5f2c2dc362f6c26ce2797fb992792b3e
kyungnankimKHT <165994108+kyungnankimKHT@users.noreply.github.com>
2024-04-18 10:47:39 +0900

    array

---
+++ 06_Array/src/com/kh/arrayList/ex/ArrayListEx2.java
@@ -0,0 +1,37 @@
+package com.kh.arrayList.ex;
+
+import java.util.ArrayList;
+
+public class ArrayListEx2 {
+//메서드
+	//기본 생성자
+	public ArrayListEx2  (){
+		
+	}
+	
+	public void method1() {
+		// 딸기 바나나 사과
+		ArrayList<String> list = new ArrayList<>();
+		
+		// add를 사용해서 딸기 바나나 사과 추가
+		list.add("딸기");
+		// 바나나 추가하기
+		list.add("사과");
+		
+		//get 0 1 2 사용해서 각 index 자리에 값 출력하기
+		
+		// set 바나나 -> 키위 수정
+		
+		// size 를 이용해서 크기 확인
+		
+		// remove 키위 삭제
+		
+		//isEmpty() 비어있는지 확인
+	}
+	
+	//메인 : 최종으로 실행하고 출력하는 메서드
+	public static void main(String[] args) {
+
+	}
+
+}
