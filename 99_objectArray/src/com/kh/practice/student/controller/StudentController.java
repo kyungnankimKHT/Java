d9b4caa7fc8ac7714d4144c212777428e665949f
kyungnankimKHT <165994108+kyungnankimKHT@users.noreply.github.com>
2024-04-19 11:27:37 +0900

    student

--- 99_objectArray/src/com/kh/practice/student/controller/StudentController.java
+++ 99_objectArray/src/com/kh/practice/student/controller/StudentController.java
@@ -22,21 +22,27 @@ public class StudentController {
 		return sArr;
 	}
 	
+	//학생들의 점수를 모두 더해서 총점을 계산
 	public int sumScore() { //점수 합한 값 전달
 		int sum = 0;
 		for(Student s : sArr) {
 			sum += s.getScore();
 		}
-		
+		//마지막으로 총합을 sumScore을 부른다면 내가 가진 점수를 보여주겠다 
 		return sum;
 	}
+	//총점을 받아서 평균 구하기
 	public double[] avgScore() {
 		double[] avgArr = new double[2];
-		avgArr[0] = sumScore();
-		avgArr[1] = avgArr[0] / sArr.length;
+		avgArr[0] = sumScore(); //위에서 만든 모든 학생의 총점 더한 값을 저장하는 공간 0
+		avgArr[1] = avgArr[0] / sArr.length; //  평균 = 총점 / 총학생수
 		return avgArr;
 	}
 
+	
+	
+	
+	
 
 	
 }
