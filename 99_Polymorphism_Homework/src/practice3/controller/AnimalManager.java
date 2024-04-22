0b66dfbc45b5dc4698e7d294c2e8d1b091fec1d8
kyungnankimKHT <165994108+kyungnankimKHT@users.noreply.github.com>
2024-04-22 14:45:32 +0900

    interface

--- 99_Polymorphism_Homework/src/practice3/controller/AnimalManager.java
+++ 99_Polymorphism_Homework/src/practice3/controller/AnimalManager.java
@@ -3,6 +3,8 @@ package practice3.controller;
 import java.util.Random;
 
 import practice3.model.vo.Animal;
+import practice3.model.vo.Cat;
+import practice3.model.vo.Dog;
 
 public class AnimalManager {
 	public static void main(String[] args) {
@@ -10,11 +12,28 @@ public class AnimalManager {
 		Animal[] animals = new Animal[5];
 		// 무작위로 
 		Random random = new Random();
-		/*
-		 각 인덱스별로 무작위로 
-		 	Dog, Cat 클래스를 이용하여 
-		 객체 생성하고 반복문으로 각 인덱스별 객체의 speak() 메소드 실행
-		 * */
+		
+		
+		for (int i = 0; i < animals.length; i++) {
+			//만약에 랜덤으로 true false 를 가져오게 한 다음 true 면 강아지
+			  //random.nextBoolean()  : 랜덤으로 거짓과 참이 나옴
+			if (random.nextBoolean()) {
+				// Dog(String name, String kinds,int weight)
+				//멍멍이 이름을 지어줄 때 멍멍이1 멍멍이2 "멍멍이" + 뒤에오는 숫자
+				//	강아지 종은 개      강아지 몸무게는 1~30 kg 까지 측정
+				animals[i] = new Dog("멍멍이" + i, "개", random.nextInt(30) + 1);
+			} else {
+				animals[i] = new Cat("야옹이" + i, "고양이", "집","검은색");
+			}
+			
+
+		}
+		//System.out.println(animals[0]);
+		
+		
+		for(Animal  a   :   animals) {
+			a.speak();
+		}
 	}
 }
 
