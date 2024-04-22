bcff28831a10af9d219b8e069cbd9ffe644d2767
kyungnankimKHT <165994108+kyungnankimKHT@users.noreply.github.com>
2024-04-22 14:32:23 +0900

    interface

---
+++ 99_Polymorphism_Homework/src/practice3/controller/AnimalManager.java
@@ -0,0 +1,27 @@
+package practice3.controller;
+
+import java.util.Random;
+
+import practice3.model.vo.Animal;
+
+public class AnimalManager {
+	public static void main(String[] args) {
+		//사이즈 5짜리 Animal 타입의 객체 배열 생성
+		Animal[] animals = new Animal[5];
+		// 무작위로 
+		Random random = new Random();
+		/*
+		 각 인덱스별로 무작위로 
+		 	Dog, Cat 클래스를 이용하여 
+		 객체 생성하고 반복문으로 각 인덱스별 객체의 speak() 메소드 실행
+		 * */
+	}
+}
+
+
+
+
+
+
+
+
