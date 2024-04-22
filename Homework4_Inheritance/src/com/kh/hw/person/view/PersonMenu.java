40acd60e8645b5c13aaf2979101898fb063b30cb
kyungnankimKHT <165994108+kyungnankimKHT@users.noreply.github.com>
2024-04-22 17:15:19 +0900

    interface

--- Homework4_Inheritance/src/com/kh/hw/person/view/PersonMenu.java
+++ Homework4_Inheritance/src/com/kh/hw/person/view/PersonMenu.java
@@ -2,10 +2,14 @@ package com.kh.hw.person.view;
 
 import java.util.Scanner;
 
+import com.kh.hw.person.controller.PersonController;
+import com.kh.hw.person.model.vo.Student;
+
 public class PersonMenu {
 	//사용자한테 숫자를 입력받고
 	Scanner sc = new Scanner(System.in);
-	
+	//1번 사람컨트롤러 가져오기
+	PersonController pc = new PersonController();
 	
 	public void mainMenu() {
 		while(true) {
@@ -20,7 +24,7 @@ public class PersonMenu {
 			switch(choice) {
 			case 1:
 				studentMenu();
-				//break;
+				break;
 			case 0:
 				System.out.println("프로그램을 종료합니다.");
 				return;
@@ -47,7 +51,7 @@ public class PersonMenu {
 				insertStudent();
 				break;
 			case 2:
-				//학생출력하기;
+				printStudent();
 				break;
 			case 0:
 				System.out.println("이전 메뉴로 이동합니다.");
@@ -83,8 +87,28 @@ public class PersonMenu {
 		System.out.print("전공 : ");
 		String major = sc.nextLine();
 		
+		// 2번째로  학생추가 넣기
+		pc.insertStudent(name, age, height, weight, grade, major);
 		System.out.println("학생이 추가되었습니다.");
 	}
+	
+	//학생이 얼마나 존재하는지 출력하기
+	public void printStudent() {
+		//학생정보가 들어있는 Student 장바구니 모양의 배열 가져오기
+		Student[] students = pc.printStudent();
+		System.out.println("=== 학생목록 ===");
+		
+		//for - each
+		for(Student s       :      students) {
+			//만약에 안에가 비어있지 않다면 학생들을 보여줄 것
+			if(  s != null ) {
+				System.out.println(s);
+			} else {
+				System.out.println("보여줄 학생 정보가 없습니다.");
+			}
+		}
+	}
+	
 }
 
 
