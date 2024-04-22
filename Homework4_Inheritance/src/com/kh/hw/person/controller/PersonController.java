8a5079107fe02f8b0404ac72f16c7a011352b87d
kyungnankimKHT <165994108+kyungnankimKHT@users.noreply.github.com>
2024-04-22 15:48:44 +0900

    interface

---
+++ Homework4_Inheritance/src/com/kh/hw/person/controller/PersonController.java
@@ -0,0 +1,5 @@
+package com.kh.hw.person.controller;
+//프로젝트 명    Homework4_Inheritance
+public class PersonController {
+
+}
---
+++ Homework4_Inheritance/src/com/kh/hw/person/view/PersonMenu.java
@@ -0,0 +1,45 @@
+package com.kh.hw.person.view;
+
+import java.util.Scanner;
+
+public class PersonMenu {
+	//사용자한테 숫자를 입력받고
+	Scanner sc = new Scanner(System.in);
+	
+	
+	public void mainMenu() {
+		while(true) {
+			System.out.println("=== 메인메뉴 ===");
+			System.out.println("1. 학생 메뉴");
+			System.out.println("0. 프로그램 종료");
+			System.out.print("메뉴선택 : ");
+			int choice = sc.nextInt();
+			sc.nextLine(); // 남아있는 enter 없애기
+			
+			//번호를 선택할 수 있는 switch 소환
+			switch(choice) {
+			case 1:
+				// 학생 메뉴
+				//break;
+			case 0:
+				System.out.println("프로그램을 종료합니다.");
+				return;
+				
+			default:
+				System.out.println("잘못된 입력입니다. 다시 입력해주세요.");
+			}
+		}
+	}
+	
+	//학생을 추가하거나 조회할 것
+	
+}
+
+
+
+
+
+
+
+
+
