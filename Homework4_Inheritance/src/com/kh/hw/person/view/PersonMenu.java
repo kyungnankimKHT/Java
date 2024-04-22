e040529b276cfec21401a718d357a545af136d89
kyungnankimKHT <165994108+kyungnankimKHT@users.noreply.github.com>
2024-04-22 16:15:56 +0900

    interface

--- Homework4_Inheritance/src/com/kh/hw/person/view/PersonMenu.java
+++ Homework4_Inheritance/src/com/kh/hw/person/view/PersonMenu.java
@@ -19,7 +19,7 @@ public class PersonMenu {
 			//번호를 선택할 수 있는 switch 소환
 			switch(choice) {
 			case 1:
-				// 학생 메뉴
+				studentMenu();
 				//break;
 			case 0:
 				System.out.println("프로그램을 종료합니다.");
@@ -34,11 +34,67 @@ public class PersonMenu {
 	//학생을 추가하거나 조회할 것
 	public void studentMenu() {
 		while(true) {
+			System.out.println("=== 학생 메뉴 ===");
+			System.out.println("1. 학생 추가");
+			System.out.println("2. 학생 출력");
+			System.out.println("0. 이전 메뉴로");
+			System.out.print("메뉴 선택 : ");
+			int choice = sc.nextInt();
+			sc.nextLine();
 			
+			switch(choice) {
+			case 1:
+				//학생추가하기;
+				break;
+			case 2:
+				//학생출력하기;
+				break;
+			case 0:
+				System.out.println("이전 메뉴로 이동합니다.");
+				return;
+			default:
+				System.out.println("잘못된 입력입니다. 다시 입력해주세요.");
+			
+			}
 		}
 	}
 	
+
+	public void insertStudent() {
+		System.out.print("이름 : ");
+		String name = sc.nextLine();
+		
+		System.out.print("나이 : ");
+		int age = sc.nextInt();
+		sc.nextLine();
+		
+		System.out.print("신장 : ");
+		double height = sc.nextDouble();
+		sc.nextLine();
+		
+		System.out.print("몸무게 : ");
+		double weight = sc.nextDouble();
+		sc.nextLine();
+		
+		System.out.print("학년 : ");
+		int grade = sc.nextInt();
+		sc.nextLine();
+		
+		System.out.print("전공 : ");
+		String major = sc.nextLine();
+		
+		System.out.println("학생이 추가되었습니다.");
+	}
 }
+
+
+
+
+
+
+
+
+
 
 
 
