0b318621117ea240e7fc86ea0b972afd06ffe4f3
kyungnankimKHT <165994108+kyungnankimKHT@users.noreply.github.com>
2024-04-23 15:26:52 +0900

    API String

---
+++ 10_Collection/src/com/kh/ListEx/ListPre.java
@@ -0,0 +1,21 @@
+package com.kh.ListEx;
+/*
+     ┌ Vector     : Java1 버전 대에서 제일 먼저 등장해서 사용된 것
+List ├ ArrayList  : 검색에 좋음 데이터 삽입과 삭제가 빈번한경우 부적합
+	 └ LinkedList : 검색엔 부적함 데이터 삽입과 삭제에는 좋음
+ 
+ * */
+
+
+public class ListPre {
+
+}
+
+
+
+
+
+
+
+
+
