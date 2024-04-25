0ef87c2db5c727046f794e0955acde71b619b0ec
kyungnankimKHT <165994108+kyungnankimKHT@users.noreply.github.com>
2024-04-25 15:47:37 +0900

    File

--- 99_11_2_입출력실습문제/src/com/kh/practice/file/run/Run.java
+++ 99_11_2_입출력실습문제/src/com/kh/practice/file/run/Run.java
@@ -1,5 +1,12 @@
 package com.kh.practice.file.run;
 
-public class Run {
+import com.kh.practice.file.view.FileMenu;
 
+public class Run {
+	public static void main(String[] args) {
+		FileMenu fm = new FileMenu();
+		while(true) {
+			fm.mainMenu();
+		}
+	}
 }
