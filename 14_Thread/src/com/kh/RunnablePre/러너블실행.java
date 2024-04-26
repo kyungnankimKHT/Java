9306632cd5bbfd472849bf5e0cca4d3d7d7759ff
kyungnankimKHT <165994108+kyungnankimKHT@users.noreply.github.com>
2024-04-26 11:16:46 +0900

    Threade

---
+++ 14_Thread/src/com/kh/RunnablePre/러너블실행.java
@@ -0,0 +1,9 @@
+package com.kh.RunnablePre;
+
+public class 러너블실행 {
+	//main 메서드 생성
+	
+	//for 문으로 스레드 5개 만든 후
+	
+	//for문을 탈출하면 메인메서드 종료일까요? 라고 출력하기
+}
---
+++ 14_Thread/src/com/kh/RunnablePre/러너블연습.java
@@ -0,0 +1,11 @@
+package com.kh.RunnablePre;
+					//implements Runnable
+public class 러너블연습 {
+//필드 int 초;
+	
+//메서드
+	//필수생성자
+	
+	//void run() sleep 1000 초 try catch this.초 + "  스레드 시작"   
+	//this.초 + 스레드 종료
+}
